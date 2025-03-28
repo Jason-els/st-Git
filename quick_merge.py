48cda5ccfb0f5679e77131ddb774dc90b3669028
Jason-els <piters40089523@gmail.com>
2025-03-28 21:18:59 +0300

    Red

--- quick_merge.py
+++ quick_merge.py
@@ -4,7 +4,9 @@ def quick_merge(list1, list2):
     p1 = 0  # указатель на первый элемент списка list1
     p2 = 0  # указатель на первый элемент списка list2
 
-    while p1 < len(list1) and p2 < len(list2):  # пока не закончился какой-нибудь из списков
+    while p1 < len(list1) and p2 < len(
+        list2
+    ):  # пока не закончился какой-нибудь из списков
         if list1[p1] <= list2[p2]:
             result.append(list1[p1])
             p1 += 1
@@ -12,11 +14,11 @@ def quick_merge(list1, list2):
             result.append(list2[p2])
             p2 += 1
 
-    if p1 < len(list1):   # прицепление остатка
+    if p1 < len(list1):  # прицепление остатка
         result += list1[p1:]
-    else:                 # иначе прицепляем остаток другого списка
+    else:  # иначе прицепляем остаток другого списка
         result += list2[p2:]
-    
+
     return result
 
 
